8045b917196d05bb1f22c2f47998009e7841462b
mfabianchm <mfchongm@hotmail.com>
2025-05-14 10:17:37 -0600

    Validations annotation added for fields in Country class

--- src/main/java/com/example/Food/Delivery/App/entities/Country.java
+++ src/main/java/com/example/Food/Delivery/App/entities/Country.java
@@ -2,6 +2,8 @@ package com.example.Food.Delivery.App.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "countries")
 public class Country {
@@ -10,6 +12,30 @@ public class Country {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "country_name", nullable = false, unique = true)
     private String countryName;
+
+    public Country() {}
+
+    public Country(String countryName) {
+        this.countryName = countryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{id=" + id + ", countryName='" + countryName + "'}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Country)) return false;
+        Country country = (Country) o;
+        return Objects.equals(id, country.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
