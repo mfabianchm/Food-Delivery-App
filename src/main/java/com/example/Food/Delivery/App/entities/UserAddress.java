1d670bc31836b5d83490364e78dcf000e7299d33
mfabianchm <mfchongm@hotmail.com>
2025-05-14 10:38:20 -0600

    Validations annotation added for fields in UserAddress class

--- src/main/java/com/example/Food/Delivery/App/entities/UserAddress.java
+++ src/main/java/com/example/Food/Delivery/App/entities/UserAddress.java
@@ -2,8 +2,10 @@ package com.example.Food.Delivery.App.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
-@Table(name = "user_address")
+@Table(name = "user_addresses")
 public class UserAddress {
 
     @Id
@@ -11,10 +13,35 @@ public class UserAddress {
     private Long id;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "address_id")
+    @JoinColumn(name = "address_id", nullable = false)
     private Address address;
+
+    public UserAddress() {}
+
+    public UserAddress(User user, Address address) {
+        this.user = user;
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAddress)) return false;
+        UserAddress that = (UserAddress) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "UserAddress{id=" + id + "}";
+    }
 }
