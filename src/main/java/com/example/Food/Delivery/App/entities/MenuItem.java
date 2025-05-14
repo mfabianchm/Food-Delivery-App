478b4e0fe4495ca8c785376a4a7d76660eb696f6
mfabianchm <mfchongm@hotmail.com>
2025-05-14 10:26:20 -0600

    Validations annotation added for fields in MenuItem class

--- src/main/java/com/example/Food/Delivery/App/entities/MenuItem.java
+++ src/main/java/com/example/Food/Delivery/App/entities/MenuItem.java
@@ -1,8 +1,10 @@
 package com.example.Food.Delivery.App.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 @Table(name = "menu_items")
@@ -13,12 +15,86 @@ public class MenuItem {
     private Long id;
 
     @Column(nullable = false)
+    @NotNull
     private String name;
 
+    @Column(nullable = false, length = 500)
+    @NotNull
+    private String description;
+
     @Column(nullable = false)
+    @NotNull
     private BigDecimal price;
 
     @ManyToOne
     @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
+
+    public MenuItem() {}
+
+    public MenuItem(String name, String description, BigDecimal price, Restaurant restaurant) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                "description=" + description +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MenuItem)) return false;
+        MenuItem that = (MenuItem) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
